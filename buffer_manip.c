d06b74c7002cc3026ae198087ae14bce1c2b9592
bari-allen <karl@hcubed.com>
2025-07-09 23:23:21 -0700

    Added two files

---
+++ buffer_manip.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+
+#define WORD_SIZE 2
+#define CRC8_POLYNOMIAL 0x31u
+#define HAS_LEADING_ONE(x) ((x & 0x80) == 0)
+
+uint8_t generate_crc(uint8_t* data) {
+    uint16_t current_byte;
+    uint8_t crc = 0xFF;
+    uint8_t bit;
+
+    for (current_byte = 0; current_byte < WORD_SIZE; ++current_byte) {
+        crc ^= data[current_byte];
+        for (bit = 0; bit < 8; ++bit) {
+            if (HAS_LEADING_ONE(crc)) {
+                crc = (crc << 1) & CRC8_POLYNOMIAL;
+            } else {
+                crc = (crc << 1);
+            }
+        }
+    }
+
+    return crc;
+}
+
+uint16_t add_uint32_to_buffer(uint8_t* buffer, uint32_t offset, uint32_t data) {
+    buffer[offset++] = (uint8_t)((data & 0xFF000000) >> 24);
+    buffer[offset++] = (uint8_t)((data & 0x00FF0000) >> 16);
+    buffer[offset] = generate_crc(&buffer[offset - WORD_SIZE]);
+    ++offset;
+
+    buffer[offset++] = (uint8_t)((data & 0x0000FF00) >> 8);
+    buffer[offset++] = (uint8_t)((data & 0x000000FF));
+    buffer[offset] = generate_crc(&buffer[offset - WORD_SIZE]);
+    ++offset;
+
+    return offset;
+}
---
+++ interactions.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <linux/i2c-dev.h>
+
+#define READ_FAILED -1
+#define WRITE_FAILED -1
+#define INIT_FAILED -1
+
+#define DEVICE_ADDRESS 0x69
+
+static int i2c_bus = 0;
+
+/**
+ * @brief Initializes the hardware and software components of the given I2C adapter
+ * 
+ * @param adapter_num the I2C adapter to initialize
+ * @return int 0 is successful or INIT_FAILED if unsuccessful
+ */
+int device_init(uint32_t adapter_num) {
+    char filename[20];
+    snprintf(filename, 19, "/dev/i2c-%d", adapter_num);
+    i2c_bus = open(filename, O_RDWR);
+
+    if (i2c_bus < 0) {
+        return INIT_FAILED;
+    }
+
+    if (ioctl(i2c_bus, I2C_SLAVE, DEVICE_ADDRESS) < 0) {
+        return INIT_FAILED;
+    }
+
+    return 0;
+}
+
+/**
+ * @brief Closes the I2C adapter
+ * 
+ */
+void device_free(void) {
+    if (i2c_bus >= 0) {
+        close(i2c_bus);
+    }
+
+    i2c_bus = 0;
+}
+
+/**
+ * @brief Writes the count number of data from data to the I2C device
+ * 
+ * @param data the data to be written
+ * @param count the amount of data to be written
+ * @return 0 if successful or WRITE_FAILED if unsuccessful
+ */
+int8_t device_write(uint8_t* data, uint16_t count) {
+    if (write(i2c_bus, data, count) != count) {
+        return WRITE_FAILED;
+    }
+
+    return 0;
+}
+
+/**
+ * @brief Reads the count number of data from the I2C device to data
+ * 
+ * @param data where the data will be written to
+ * @param count the amount of data to be read
+ * @return 0 if successful or READ_FAILED otherwise
+ */
+int8_t device_read(uint8_t* data, uint16_t count) {
+    if (read(i2c_bus, data, count) != count) {
+        return READ_FAILED;
+    }
+
+    return 0;
+}
